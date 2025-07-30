360b5c5f432809fa6c73016bfd11a7bfcbe65015
CBI Analytics <cbi@email.com>
2025-07-30 08:37:42 +0200

    tutorial 5

---
+++ Video 5/posts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from posts import views as views
+
+urlpatterns = [
+   path('',views.home, name='home'),
+   path('blogposts',views.blogposts, name='blogposts')
+]
