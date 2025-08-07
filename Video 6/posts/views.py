2a15a9cecea33708f167a113ec4456cb669b27a4
CBI Analytics <cbi@email.com>
2025-08-07 19:03:05 +0200

    video 6

---
+++ Video 6/posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(request):
+    title = "this is my title"
+    context = {'title': title}
+    return render(request,'posts/home.html',context)
+
+
+def blogposts(request):
+    context = {}
+    return render(request,'posts/blogposts.html',context)
