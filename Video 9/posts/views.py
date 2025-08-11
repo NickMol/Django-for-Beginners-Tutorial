62a5ae2fbfde612401735c51a75df830aa8b4354
CBI Analytics <cbi@email.com>
2025-08-11 15:41:03 +0200

    video 9

---
+++ Video 9/posts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from .forms import BlogForm
+from .models import BlogPost
+
+def home(request):
+    blogs = BlogPost.objects.all().order_by('-created_on')
+    blogscount = blogs.count()
+
+    context = {
+        'blogs':blogs,
+        'blogscount':blogscount
+    }
+    return render(request,'posts/home.html',context)
+
+
+def blogposts(request):
+    context = {}
+    return render(request,'posts/blogposts.html',context)
+
+
+def create(request):
+    form = BlogForm()
+
+    if request.method == "POST": 
+        form = BlogForm(request.POST)
+        if form.is_valid(): 
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+
+            blog = BlogPost(
+                title=title, 
+                content = content
+            )
+
+            blog.save()
+
+            return redirect('blogposts')
+
+    context = {'form':form}
+    return render(request,'posts/create.html',context)
